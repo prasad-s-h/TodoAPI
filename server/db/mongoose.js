bf3e089496cc96ec97a0f5cd228b0c63a3caad47
prasad-s-h <prasads367@gmail.com>
2019-02-21 14:13:55 +0530

    final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,19 @@
 
 const mongoose = require('mongoose');
+const db = {
+    localhost: 'mongodb://localhost:27017/ToDos',
+    mongoAtlas: 'mongodb+srv://all:gwmPwcM487rIziB9@cluster0-e7f5l.mongodb.net/test?retryWrites=true'
+  };
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ToDos', {useNewUrlParser: true});
+mongoose.connect( db.mongoAtlas , {useNewUrlParser: true} )
+.then( () => {
+    console.log('connected to db');
+})
+.catch( (e) => {
+    console.log('failed to connect due to following error');
+    console.log(e);
+});
 
 module.exports = {
     mongoose
